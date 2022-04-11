a2d95b91d4169b7b3f35550b1d93fff8e010aff4
Lore <kanelillym@riseup.net>
2022-04-11 09:35:02 -0700

    Add get input loop

--- pyForth.py
+++ pyForth.py
@@ -40,8 +40,38 @@ def add():
     val2 = THE_STACK.pop()
     THE_STACK.push(val1+val2)
 
-THE_STACK.push(10)
-THE_STACK.push(11)
-dot_s()
-add()
-dot_s()
+def eval_word(key_word):
+    # if COMMENT_MODE only process comment_end
+    if is_number(key_word):
+        THE_STACK.push(int(key_word))
+    else:
+        match key_word.split():
+            case ["bye"]:
+                bye()
+                return
+            case [*words]:
+               pass
+        if key_word in THE_DICTIONARY:
+            word_value = THE_DICTIONARY[key_word]
+            if len(word_value.split()) == 1:
+                word_value.split()[0]()
+            else:
+                eval_string(word_value)
+        else:
+            # process undefined word error
+            pass
+
+def main():
+    print("Welcome to PyForth!")
+    while True:
+        data = input(">> ")
+        data_array = data.split(" ")
+        print(data_array)
+        for word in data_array:
+            eval_word(word)
+        match data_array:
+            case ['bye']:
+                print("byebye!")
+                break
+
+main()
